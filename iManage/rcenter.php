57cf13a068016796ab2e6701ce026a09321c4730
E.Radbel <E.Radbel@gmail.com>
2011-09-08 07:08:19 +0000

    Added center of a welcom screen (without jscrpt)

---
+++ iManage/rcenter.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html>
+<style type="text/css">
+
+.center-right
+{
+width:450px;
+height:400px;
+/*background-color:rgb(220,220,220);*/
+text-align:left;
+left:50px;
+top:10px;
+position:relative;
+padding:0px;
+}
+
+.head
+{
+border:1px;
+    border-style:outset;
+    border-bottom-color:rgb(50,195,255);
+    border-right-color:rgb(80,200,255);
+    border-top-color:rgb(92,205,255);
+    border-left-color:rgb(80,200,255); 
+margin-top:1px;
+margin-buttom:1px;    
+margin-left:1px;
+margin-right:1px;
+width:448px;
+height:22px;
+background-image: url('h_back.png');
+background-repeat: repeat-x;
+text-align:left;
+left:0px;
+top:0px;
+position:relative;
+padding:0px;
+}
+
+.buttom
+{
+width:450px;
+height:378px;
+background-image: url('centerdrad.png');
+background-repeat: repeat-y;
+left:1px;
+top:0px;
+position:relative;
+padding:0px;
+}
+
+
+</style>
+
+
+<body>
+<div class="center-right">
+   <div class="head">   
+     <form style="font-size:11px; position:relative; top:0px;" method="get" action="form_action.asp" method="get" accept-charset="UTF-8" >
+     &nbsp;&nbsp;<input type="image" style="position:relative; top:3px;"  src="login2.png" alt="login_img" width="44" height="16" /> 
+     <font style="font-size:10px;" face="arial" color="black">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     UserName:&nbsp;<input type="text" style="font-size:11px; position:relative; top:-2px;" size="15" maxlength="15" />&nbsp;&nbsp;&nbsp;&nbsp;
+     Password:&nbsp;<input type="password" style="font-size:11px; position:relative; top:-2px;" size="15" maxlength="15" margin-top:0px; margin-buttom:0px; margin-right:10px; margin-left:0px;/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <input type="image" style="position:relative; top:3px;"  src="submit.png" alt="Submit" width="18" height="18" /><br />
+     
+     </form>
+   </div>
+   <div class="buttom">
+   
+   </div>
+</div>
+</body>
+</html>
