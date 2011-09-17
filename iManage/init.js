27358f1d52ad34a7959f60b53b3efa367df41302
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-09-17 13:52:37 +0000

    Login Page + login Verifier + JavaScript+jQuery+Ajax :) Connection to database + login verification.

---
+++ iManage/init.js
@@ -0,0 +1,41 @@
+function init()
+{
+	setLogin();
+}
+
+function setLogin()
+{
+	$("#login_form").submit(function()
+	{
+		//remove all the class add the messagebox classes and start fading
+		$("#msgBox").removeClass().addClass('messagebox').text('Validating....').fadeIn(1000);
+		//check the username exists or not from ajax
+		$.post("loginVerifier.php",{ username:$('#username').val(),password:$('#password').val(),rand:Math.random() } ,function(data)
+        {
+		  if(data=='yes') //if correct login detail
+		  {
+		  	$("#msgBox").fadeTo(200,0.1,function()  //start fading the messagebox
+			{ 
+			  //add message and change the class of the box and start fading
+			  $(this).html('Logging in.....').addClass('messageboxok').fadeTo(900,1,
+              function()
+			  { 
+			  	 //redirect to secure page
+				 document.location='index.php';
+			  });
+			  
+			});
+		  }
+		  else 
+		  {
+		  	$("#msgBox").fadeTo(200,0.1,function() //start fading the messagebox
+			{ 
+			  //add message and change the class of the box and start fading
+			  $(this).html('Your login information incorrect...').addClass('messageboxerror').fadeTo(900,1);
+			});		
+          }
+				
+        });
+ 		return false; //not to post the  form physically
+	});
+}
