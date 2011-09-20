ac2486c2fb2a3420fd4cc32bfd97b8304c8bbf47
DJeansGirl <djeansgirl@gmail.com>
2011-09-20 14:01:22 +0000

    Field changes only. Still one field is missing - a checkbox with a text - "I would like to be a premium user", please add it Styles also have to be changed in a css file.

--- iManage/signuppage.php
+++ iManage/signuppage.php
@@ -28,53 +28,52 @@
             <div id="content-middle">  
             <table class="shead" border="0" width="98%">
               <tr height="30">
-               <td align="left" valign="center"><font style="font-size:13px; position:relative;font-weight:bold; text-decoration:none; text-decoration:none;
+               <td align="left" valign="center" width="10"><font style="font-size:13px; position:relative;font-weight:bold; text-decoration:none;
                    color:#FE2712" face="arial"> *Please fill in all fields below</font></td>
+               <td align="left" valign="center" width="20"></td>
               </tr>
+              <tr height="30">
+               <td align="left" valign="center" width="10"><font style="font-size:13px; ;font-weight:bold; text-decoration:none; 
+                   color:gray;" face="arial"> Personal details:</font></td>
+               <td align="left" valign="center" width="20"><font style="font-size:13px;;font-weight:bold; text-decoration:none; 
+                   color:gray;" face="arial">Login information:</font></td>
+              </tr>
+              
+              
              <font style="font-size:13px; position:relative;" face="arial" color="black">
              <table class="sbody" border="0" align="left" valign="top">                
                       <tr>
+                          <td>First Name:</td>
+                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="30" id="usrfname" /></td>
+                          <td></td>
                           <td>UserName:</td>
-                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="30" id="usrnmfield" /></td>
-                          <td></td>
-                          <td>Credit card:</td>
-                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="16" id="usrccn" /></td>
+                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="16" id="usrnmfield" /></td>
                       </tr>
                       <tr>
+                          <td>Last Name:</td>
+                          <td><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrlname"/></td>
+                          <td></td>
                           <td>Password:</td>
-                          <td><input type="password" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrpsw"/></td>
-                          <td></td>
-                          <td>ID:</td>
-                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="20" id="usrid" /></td>
+                          <td><input type="password" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="20" id="usrpsw" /></td>
                       </tr>
                       <tr>
+                          <td>Date of birth:</td>
+                          <td width="20px" height="16"><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrdob"/></td>
+                          <td></td>
                           <td>Confirm Password:</td>
-                          <td width="20px" height="16"><input type="password" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrpswconf"/></td>
-                          <td></td>
-                          <td>Bank Number:</td>
-                          <td><input type="text" style="font-size:11px; position:relative; top:0px;" size="14" maxlength="20" id="usrbla" /></td>
+                          <td><input type="password" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="20" id="usrpswconf" /></td>
                       </tr>
                       <tr>
-                          <td>EMail:</td>
+                          <td>eMail address:</td>
                           <td width="20px" height="16"><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrmail"/></td>
                       </tr>
-                      <tr>
-                          <td>Age:</td>
-                          <td width="20px" height="16"><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="8" maxlength="3" id="usrage"/></td>
-                      </tr>
-                      <tr>
-                          <td>Country:</td>
-                          <td width="20px" height="16"><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="usrmail"/></td>
-                      </tr>
-                      <tr>
-                          <td>Bla Bla:</td>
-                          <td width="20px" height="16"><input type="text" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="25" id="usrmail"/></td>
-                      </tr>                
+ 
+               
                 </table>
                 </font>
                 <table class="sfoot" border="0" width="98%" cellspacing="20">
                       <tr align="left">
-                          <td><font style="font-size:13px; position:relative; text-decoration:none; text-decoration:none" face="arial" align="center" valign="bottom">You've got ROBBED! HA HA HA...</font></td>      
+                          <td><font style="font-size:13px; position:relative; text-decoration:none;  face="arial" align="center" valign="bottom">You've got ROBBED! HA HA HA...</font></td>      
                       </tr>
                       <tr >    
                           <td align="right" valign="bottom"><input type="image" src="images/signbuttpix.png" style="position:relative; right:-12px; top:0px;" alt="submit"  
