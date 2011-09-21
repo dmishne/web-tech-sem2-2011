18fb35359268a5fee5f8585a0f73916eda3b683d
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-09-21 16:33:41 +0000

    Change implementation of PHP-MYSQL interaction to support PHP5.
    Bonus:
    	Icon added.

--- trunk/iManage/index.php
+++ trunk/iManage/index.php
@@ -1,10 +1,15 @@
+<?php session_start();
+if (isset($_GET['logout']) && $_GET['logout'] == 1) {
+	session_unset();
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html>
-
 <head>
 	<title> iManage </title>
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
-	
+	<link rel="icon" href="images/logo.ico" />
+	<link rel="apple-touch-icon" href="images/icon_apple.png" />
 	<?php include "include.php" ?>
 </head>
 
--- trunk/iManage/login.php
+++ trunk/iManage/login.php
@@ -1,9 +1,13 @@
+<?php session_start();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html>
 
 <head>
 	<title> iManage </title>
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<link rel="icon" href="images/logo.ico" />
+	<link rel="apple-touch-icon" href="images/icon_apple.png" />
 	<?php include "include.php" ?>
 	
 	<script type="text/javascript">
