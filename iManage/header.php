8f7ff38fe76bfce436de529f6cb8bd3568bb1e28
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-09-21 16:33:41 +0000

    Change implementation of PHP-MYSQL interaction to support PHP5.
    Bonus:
    	Icon added.

--- iManage/header.php
+++ iManage/header.php
@@ -2,14 +2,7 @@
 		<div id="logo"> <img id="underConstruction" src="images/under-construction.png" /> </div>
 		<div id="logoHeading">Some sentence </div>
 		<div id="loginBox">
-		<?PHP
-			if (isset($_GET['logout']) && $_GET['logout'] == 1) {
-				session_start();
-				session_unset();
-				session_destroy();
-				header ('Location: index.php');
-			}
-			session_start();
+		<?php
 			if (!(isset($_SESSION['login']) && $_SESSION['login'] != '0')) {
 				echo "<a class=\"blue button medium\" href=\"login.php\"> Login </a>
 				      <br />
--- iManage/loginVerifier.php
+++ iManage/loginVerifier.php
@@ -1,21 +1,21 @@
 <?php session_start();
 
 //Connect to database from here
-$connection = mysql_connect("remote-mysql4.servage.net", "webtech", "12345678"); 
-if (!$connection) {
-    die('Could not connect: ' . mysql_error());
+$connection = new mysqli("remote-mysql4.servage.net", "webtech", "12345678"); 
+if (mysqli_connect_errno()) {
+    die('Could not connect: ' . mysqli_connect_error());
 }
 
-mysql_select_db('webtech'); 
+$connection->select_db('webtech');
 
 $username=htmlspecialchars($_POST['username'],ENT_QUOTES);
 $password=md5($_POST['password']);
 
 //now validating the username and password
-$res = mysql_query("CALL doLogin('$username', '$password')");
-$userDetails = mysql_fetch_array($res);
+$res = $connection->query("CALL doLogin('$username', '$password')") or die(mysqli_error());
+$userDetails = $res->fetch_array(MYSQLI_ASSOC);
 
-if(mysql_num_rows($res)>0){
+if($res->num_rows > 0){
 	$_SESSION['login']    = "1";
 	$_SESSION['username'] = $username;
 	echo "yes";
