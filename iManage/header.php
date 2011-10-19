e14d6ade01773782f8197371860845a5a1d992ce
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-19 18:51:04 +0000

    Signup rewritten + Signup Validation.

--- iManage/header.php
+++ iManage/header.php
@@ -9,10 +9,11 @@
 					  <a class=\"blue buttonStyle medium\" href=\"signuppage.php\" style=\"margin-top:5px\"> Sign Up </a>";
 			}
 			else {
-				echo "Hey {$_SESSION['username']},
+				echo "Hey {$_SESSION['firstname']},
 					  <br />
-					  <a class=\"blue buttonStyle medium\" href=\"index.php?logout=1\"> Logout </a>
-						<br /> Your balance is: {$_SESSION['balance']}" ;
+					  Balance: {$_SESSION['balance']} 
+					  <br />
+					  <a class=\"blue buttonStyle medium\" href=\"index.php?logout=1\"> Logout </a>";
 				
 			}
 		?>
--- iManage/signupVerifier.php
+++ iManage/signupVerifier.php
@@ -8,31 +8,68 @@ if (mysqli_connect_errno()) {
 
 $connection->select_db('webtech');
 
+$errors = array();
+$REG = 1;
+
 $firstname=htmlspecialchars($_POST['firstname'],ENT_QUOTES);
+if(strlen($firstname) < 3 || $firstname != $_POST['firstname'])
+{
+	$error['firstname'] = 1;
+	$REG = 0;
+}
+
 $lastname=htmlspecialchars($_POST['lastname'],ENT_QUOTES);
+if(strlen($lastname) < 3 || $lastname != $_POST['lastname'])
+{
+	$error['lastname'] = 1;
+	$REG = 0;
+}
+
 $username=htmlspecialchars($_POST['username'],ENT_QUOTES);
+if(strlen($username) < 3 || $username != $_POST['username'])
+{
+	$error['username'] = 1;
+	$REG = 0;
+}
+
 $day=htmlspecialchars($_POST['signUpDay'],ENT_QUOTES);
 $month=htmlspecialchars($_POST['signUpMonth'],ENT_QUOTES);
 $year=htmlspecialchars($_POST['signUpYear'],ENT_QUOTES);
+if(!checkdate(intval($month),intval($day),intval($year)))
+{
+	$error['date'] = 1;
+	$REG = 0;
+}
+
 $password=md5($_POST['userpassword']);
 $passwordconfirm=md5($_POST['userpasswordconfirm']);
-$email=htmlspecialchars($_POST['email'],ENT_QUOTES);
-$res = "ERROR";
+if($password != $passwordconfirm)
+{
+	$error['password'] = 1;
+	$REG = 0;
+}
 
-if($password == $passwordconfirm)
+$email=htmlspecialchars($_POST['email'],ENT_QUOTES);
+
+if($REG == 1)
 {
 	$res = $connection->query("CALL register('$username','$firstname','$lastname','$year-$month-$day',1,'$email','$password')") or die(mysqli_error());
 	$userDetails = $res->fetch_array(MYSQLI_NUM);
 	if($userDetails[0] == 0)
 	{
-		$res = "OK";
+		header("location:index.php");
+	}
+	else
+	{
+		$error['Reg'] = 1;
+		$REG = 0;
 	}
 }
 
-echo $res;
-
-
-
-
+if($REG == 0)
+{
+	$_SESSION['signupError'] = $error;
+	header("location:signuppage.php");
+}
 
 ?>
