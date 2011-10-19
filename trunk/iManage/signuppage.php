5c0d944cb1833e3b9f79011f4252382925c21c98
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-19 18:51:04 +0000

    Signup rewritten + Signup Validation.

--- trunk/iManage/signuppage.php
+++ trunk/iManage/signuppage.php
@@ -28,63 +28,108 @@
 			Sign Up
 			</div>
 			
-            <div id="content-middle">  
-            <table class="shead" border="0" width="98%">
-              <tr>
-               <td align="left" width="10"><font style="font-size:13px; position:relative;font-weight:bold; text-decoration:none;
-                   color:#FE2712" face="arial"> *Please fill in all fields below</font></td>
-               <td align="left" width="20"></td>
-              </tr>
-              <tr><td></td></tr>
-              <tr>
-               <td align="left" width="10"><font style="font-size:13px; ;font-weight:bold; text-decoration:none; 
-                   color:gray;" face="arial"> Personal details:</font>
-               <font style="font-size:13px; font-weight:bold; text-decoration:none; 
-                   color:gray; margin-left:160px;" face="arial">Login information:</font></td>
-              </tr>
-              <tr><td></td></tr>
-             <tr> <td>
-             <form method="post" action="signupVerifier.php" id="signup_form">
-	             <table class="sbody" border="0" align="left" valign="top" style="font-family:"arial", arial, sans-serif; font-size:13px; position:relative;">                
-	                      <tr>
-	                          <td>First Name:</td>
-	                          <td><input type="text" class="inpt" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="30" id="firstname" name="firstname" /></td>
-	                          <td></td>
-	                          <td>UserName:</td>
-	                          <td><input type="text" class="inpt" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="16" id="username" name="username" /></td>
-	                      </tr>
-	                      <tr>
-	                          <td>Last Name:</td>
-	                          <td><input type="text" class="inpt" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="lastname" name="lastname"/></td>
-	                          <td></td>
-	                          <td>Password:</td>
-	                          <td><input type="password" class="inpt" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="20" id="userpassword" name="userpassword" /></td>
-	                      </tr>
-	                      <tr>
-	                          <td>Date of birth:</td>
-	                          <td width="20px" height="16">
-	                          		<input type="text" size="1" maxlength="2" class="inpt" id="signUpDay"   name="signUpDay" />
-	                          		<input type="text" size="1" maxlength="2" class="inpt" id="signUpMonth" name="signUpMonth"/>
-	                          		<input type="text" size="1" maxlength="4" class="inpt" id="signUpYear"  name="signUpYear"/>
-	                          </td>
-	                          <td></td>
-	                          <td>Confirm Password:</td>
-	                          <td><input type="password" class="inpt" style="font-size:11px; position:relative; top:0px;" size="30" maxlength="20" id="userpasswordconfirm" name="userpasswordconfirm" /></td>
-	                      </tr>
-	                      <tr>
-	                          <td>eMail address:</td>
-	                          <td width="20px" height="16"><input type="text" class="inpt" style="font-size:11px; position:relative; right:0px; top:0px;" size="30" maxlength="30" id="email" name="email"/></td>
-	                      </tr>
-	                      <tr><td><br /><br />
-	                      <input name="signup" id="signup" type="submit" value="Sign Up" class="blue button medium" style="min-width:100px;" />
-	                      </td></tr>
-	                </table>
-             	</form>
-             	</td>
-             	</tr>
-            </table> 
-           </div>  
-          </div>
+            <div id="content-middle">
+	            <?php 
+	            	if((isset($_SESSION['signupError'])))
+	            	{
+	            		$REG = 0;
+	            		$error = $_SESSION['signupError'];
+	            		echo "<div class=\"errorGrey\"> Cannot complete registration. Please review the following: </div>";
+	            		if($error['Reg']){
+	            			echo "<div class=\"errorGrey\"> User already exist! </div>";
+	            		}
+	            		
+	            	}
+	            ?>
+	            <form method="post" action="signupVerifier.php" id="signup_form">
+	            <div id="signup_personal">
+		            <table>
+		            	<tr>
+		            		<th> Personal details: </th>
+		            	</tr>
+		            	<tr>
+		            		<td> First Name: </td>
+		            		<td><input type="text" class="inpt" style="position:relative; top:0px;" size="30" maxlength="30" id="firstname" name="firstname" /></td>
+		            	</tr>
+		            	<?php 
+		            		if(isset($error['firstname']) && $error['firstname'] = 1){
+		            			echo "<tr> <td COLSPAN=2> <div class=\"error\"> Aleast 2 letters required. </div> </td> </tr>";
+		            		}
+		            	?>
+		            	<tr>
+		            		<td> Last Name: </td>
+		            		<td><input type="text" class="inpt" style="position:relative; right:0px; top:0px;" size="30" maxlength="30" id="lastname" name="lastname"/></td>
+		            	</tr>
+		               	<?php 
+		            		if(isset($error['lastname']) && $error['lastname'] = 1){
+		            			echo "<tr COLSPAN=2> <td COLSPAN=2> <div class=\"error\"> Aleast 2 letters required. </div> </td> </tr>";
+		            		}
+		            	?>
+		            	<tr>
+		            		<td> Date of Birth: </td>
+		            		<td style="text-align: justify;">
+		                   		<input type="text" size="3" maxlength="2" class="inpt" id="signUpDay"   name="signUpDay" />
+		 	                    <input type="text" size="3" maxlength="2" class="inpt" id="signUpMonth" name="signUpMonth"/>
+		 	                    <input type="text" size="5" maxlength="4" class="inpt" id="signUpYear"  name="signUpYear"/>
+	 	                    </td>
+		            	</tr>
+		            	<?php 
+		            		if(isset($error['date']) && $error['date'] = 1){
+		            			echo "<tr> <td COLSPAN=2> <div class=\"error\"> Date incorrect! Example: 31 12 2011 </div> </td> </tr>";
+		            		}
+		            	?>
+		            	<tr>
+		            		<td> EMail Address: </td>
+		            		<td width="20px" height="16"><input type="text" class="inpt" style="position:relative; right:0px; top:0px;" size="30" maxlength="30" id="email" name="email"/></td>
+		            	</tr>
+		            </table>
+		        </div>   
+		        <div id="signup_login">
+		         	<table>
+			         	<tr>
+			         		<th> Login information: </th>
+			         	</tr>
+			         	<tr>
+			         		<td> Username: </td>
+			         		<td><input type="text" class="inpt" style="position:relative; top:0px;" size="30" maxlength="16" id="username" name="username" /></td>
+			         	</tr>
+			         	<?php 
+		            		if(isset($error['username']) && $error['username'] = 1){
+		            			echo "<tr COLSPAN=2> <td COLSPAN=2> <div class=\"error\"> Aleast 3 letters required. </div> </td> </tr>";
+		            		}
+		            	?>
+			         	<tr>
+			         		<td> Password: </td>
+			         		<td><input type="password" class="inpt" style="position:relative; top:0px;" size="30" maxlength="20" id="userpassword" name="userpassword" /></td>
+			         	</tr>
+			         	<?php 
+		            		if(isset($error['password']) && $error['password'] = 1){
+		            			echo "<tr COLSPAN=2> <td COLSPAN=2> <div class=\"error\"> Passwords are incorrect or doesn't match </div> </td> </tr>";
+		            		}
+		            	?>
+			         	<tr>
+			         		<td> Confirm Password: </td>
+			         		<td><input type="password" class="inpt" style="position:relative; top:0px;" size="30" maxlength="20" id="userpasswordconfirm" name="userpasswordconfirm" /></td>
+			         	</tr>
+			         	<?php 
+		            		if(isset($error['password']) && $error['password'] = 1){
+		            			echo "<tr COLSPAN=2> <td COLSPAN=2> <div class=\"error\"> Passwords are incorrect or doesn't match </div> </td> </tr>";
+		            		}
+		            	?>
+			       	</table>
+		        </div>
+		        <br />
+		        <br />
+		        <br />
+		        <input style="position:absolute; margin: 200px 0px 0px 0px;" name="signup" id="signup" type="submit" value="Sign Up" class="blue button medium" style="min-width:100px;" />
+		         
+	            </form>
+	
+        	</div>  
+		</div>
+        <?php 
+			unset($_SESSION['signupError']);
+        ?>
 	</div>
 	
 	
