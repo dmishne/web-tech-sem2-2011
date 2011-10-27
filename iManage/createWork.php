2cc68ffcd8d1a0c762102d8f813c2894df64a8cb
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-27 12:56:50 +0000

    Complete basic form of create work. Text area not has the same width as other filds on Firefox. Known bug: https://bugzilla.mozilla.org/show_bug.cgi?id=33654.
    You've to be logged in to see this page.

--- iManage/createWork.php
+++ iManage/createWork.php
@@ -1,4 +1,8 @@
 <?php session_start();
+// Verifies that user has logged in. Redirect to login page in case not logged in.
+if (!(isset($_SESSION['login']) && $_SESSION['login'] != '0')) {
+	header("location:login.php");
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html>
@@ -28,9 +32,30 @@
 		           Create Work Information
 			</div>
 			<div id="content-middle">
-		           Content Here <br />
-		           more content <br />
-		           even more content
+				   <form  method="post" action="" id="createWork_form">
+			           <div id="createWork" class="greyCube">
+				           <table style="margin-left:auto; margin-right:auto; text-align:left;">
+				           		<tr style="text-align:center;"> 
+				           			<th colspan=2> Create Work Form </th> 
+				           		</tr>
+				           		<tr>
+				           			<td> Name: </td>
+				           			<td> <input type="text" class="inpt" style="position:relative; top:0px;" size="30" maxlength="16" id="creatework_jobsname" name="creatework_jobsname" /> </td>
+				           		</tr>
+				           		<tr>
+				           			<td> Wage Per Hour: </td>
+				           			<td> <input type="text" class="inpt" style="position:relative; top:0px;" size="30" maxlength="16" id="creatework_wagehour" name="creatework_wagehour" /> </td>
+				           		</tr>
+				           		<tr>
+				           			<td> Notes/Description: </td>
+				           			<td><textarea name="creatework_desc" rows="8" cols="32" class="inpt"> </textarea></td>
+				           		</tr>
+				           </table>
+			           </div>
+			           <div style="clear:both; width:100%; margin: 60px 0px 0px 0px; text-align:center;">
+			           	   <input name="Submit" id="submit" type="submit" value="Create Work" class="blue button medium" style="min-width:100px;" />
+			           </div>
+		           </form>
 			</div>
 		</div>
 	</div>
