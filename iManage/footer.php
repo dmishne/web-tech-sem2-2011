1892b9ed4c054a957227a5dfe8cd14c069bbcdbd
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-29 18:26:56 +0000

    Add meta tag to all: content-type , content:"text/html; charset=utf-8". The document located at <http://imng.me/> was successfully checked as XHTML 1.0 Transitional. This means that the resource in question identified itself as "XHTML 1.0 Transitional" and that we successfully performed a formal validation of it.
    W3C icon added

--- iManage/footer.php
+++ iManage/footer.php
@@ -1,1 +1,6 @@
 <p> &#169; 2011. All Rights Reserved. </p>
+<p>
+	<a href="http://validator.w3.org/check?uri=referer">
+		<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
+	</a>
+</p>
