b8e85beecd9287e284d81c05d665266cbdf8a567
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-29 17:05:44 +0000

    bug fix

--- iManage/index.php
+++ iManage/index.php
@@ -4,7 +4,7 @@ if (isset($_GET['logout']) && $_GET['logout'] == 1) {
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title> iManage </title>
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
