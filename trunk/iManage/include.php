61d855132d2797303b4cb4f055333168259dd615
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-10-29 17:05:44 +0000

    bug fix

--- trunk/iManage/include.php
+++ trunk/iManage/include.php
@@ -5,7 +5,7 @@
 
 
 <script type="text/javascript" src="jquery-1.6.4.min.js"></script>
-<script src="jquery.cookie.js"></script>
+<script type="text/javascript" src="jquery.cookie.js"></script>
 <script type="text/javascript" src="menu.js"></script>
 <script type="text/javascript" src="init.js"></script>
 <script type="text/javascript" src="slidetgl.js"></script>
