63943b3ef1cc5a6a931f84cafe32f89aefb5fd12
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-11-06 20:34:10 +0000

    1. Fixing balance update in login box. 2. Style balance info. if balance >= 0 then green else red. 3. Add tooltip to balance saying "Balance".

--- iManage/header.php
+++ iManage/header.php
@@ -2,11 +2,11 @@
 		<div id="logo">  <!-- <img id="underConstruction" src="images/under-construction.png" /> --> </div> 
 		<div id="logoHeading">Imanage </div>
 		<div id="loginBox">
-		<?php
+		<?php 
 			if (!(isset($_SESSION['login']) && $_SESSION['login'] != '0')) {
-				echo "<a class=\"blue buttonStyle medium\" href=\"login.php\"> Login </a>
+				echo "<a style=\"float:left; margin:5px auto 5px 5px;\" class=\"blue buttonStyle medium\" href=\"login.php\"> Login </a>
 				      <br />
-					  <a class=\"blue buttonStyle medium\" href=\"signuppage.php\" style=\"margin-top:5px\"> Sign Up </a>";
+					  <a style=\"float:left; margin:5px auto 5px 5px;\" class=\"blue buttonStyle medium\" href=\"signuppage.php\" style=\"margin-top:5px\"> Sign Up </a>";
 			}
 			else {
 				if ($_SESSION['update'])
@@ -16,19 +16,24 @@
 						die('Could not connect: ' . mysqli_connect_error());
 					}
 					$connection->select_db('webtech');
-					$username = $_SESSION['login'];
+					$username = $_SESSION['username'];
 					$newres = $connection->query("CALL getBalance('$username')") or die(mysqli_error());
 					$bal = $newres->fetch_array(MYSQLI_NUM);
 					if($newres->num_rows > 0) {
 						$_SESSION['balance'] = $bal[0];
 						$_SESSION['update'] = 0;
 					}
-					
+				}
+				$title = "title=\"Balance\"";
+				if ($_SESSION['balance'] < 0){
+					$div = "<div style=\"white-space: nowrap; \" class=\"redh roundedh meduim\" {$title}>";
+				}				
+				else {
+					$div = "<div style=\"white-space: nowrap; \" class=\"greenh roundedh meduim\" {$title}>";
 				}
 				echo "Hey {$_SESSION['firstname']},
 					  <br />
-					  Balance: {$_SESSION['balance']} 
-					  <br />
+					  {$div} &Sigma; {$_SESSION['balance']}$ </div>
 					  <a class=\"blue buttonStyle medium\" href=\"index.php?logout=1\"> Logout </a>";
 				
 			}
