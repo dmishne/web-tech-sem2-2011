402569265e9b2cffed8cbe593e856957d25f1015
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-12-21 09:30:02 +0000

    Commit login changes to support user lock/delete.

--- iManage/loginVerifier.php
+++ iManage/loginVerifier.php
@@ -10,6 +10,11 @@ if (mysqli_connect_errno()) {
 
 $connection->select_db('webtech');
 
+/*
+$_POST['username'] = 'daniel';
+$_POST['password'] = '123123123';
+*/
+
 $username=htmlspecialchars($_POST['username'],ENT_QUOTES);
 $password=md5($_POST['password']);
 
@@ -17,7 +22,21 @@ $password=md5($_POST['password']);
 $res = $connection->query("CALL doLogin('$username', '$password')") or die(mysqli_error());
 $userDetails = $res->fetch_array(MYSQLI_NUM);
 
-if($res->num_rows > 0){
+
+if($userDetails[0] == 'locked account')
+{
+	echo "locked";
+}
+else if ($userDetails[0] == 'Account Deleted')
+{
+	echo "deleted";
+}
+else if ($userDetails[0] == 'Incorrect account details')
+{
+	echo "incorrect";
+}
+else if ($res->num_rows > 0)
+{
 	$_SESSION['login']    = "1";
 	$_SESSION['username'] = $username;
 	$_SESSION['firstname'] = $userDetails[0];
@@ -35,14 +54,19 @@ if($res->num_rows > 0){
 		}
 	}
 	
-	
 	$newres = $connection->query("CALL getBalance('$username')") or die(mysqli_error());
 	$bal = $newres->fetch_array(MYSQLI_NUM);
 	if($newres->num_rows > 0)
+	{
 		$_SESSION['balance'] = round($bal[0],2);
+	}
+	else {
+		$_SESSION['balance'] = 0;
+	}
 	echo "yes";
 }
-else{
-	echo "no"; //Invalid Login
+else
+{
+	echo "incorrect";
 }
 ?>
