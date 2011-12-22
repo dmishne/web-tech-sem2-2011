54dabc83019c4400a702758cb68837fe6f014d5b
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-12-22 08:50:44 +0000

    Stock update - Still not working

--- iManage/stockVerifier.php
+++ iManage/stockVerifier.php
@@ -18,56 +18,93 @@ session_start();
 
 include "ini.php";
 
-$error = 0;
+$action=htmlspecialchars($_POST['action'],ENT_QUOTES);
 
-$symbol=htmlspecialchars($_POST['symbol'],ENT_QUOTES);
-$amount=htmlspecialchars($_POST['amount'],ENT_QUOTES);
-$curr_value=0;
-$username= $_SESSION['username'];
-
-$tempCsvString = file_get_contents("http://download.finance.yahoo.com/d/quotes.csv?s=". $symbol ."&f=snp");
-$stockData = csvToArray($tempCsvString);
-
-if(isset($stockData[0][2]) && is_numeric($stockData[0][2]))
+if($action == "add")
 {
-	$curr_value = floatval($stockData[0][2]);
-}
-else
-{
-	$error++;
-}
-
-if (is_numeric($amount) && floatval($amount) > 0)
-{
-	$amount = floatval($amount);
-} 
-else {
-	$error++;
-}
-
-if ($error == 0)
-{
-	$connection = new mysqli($serverInfo["address"], $serverInfo["username"], $serverInfo["password"]);
-	if (mysqli_connect_errno()) {
-		die('Could not connect: ' . mysqli_connect_error());
-	}
-	$connection->select_db($serverInfo["db"]);
-	$year_mon_day = date("Y-m-d");
-	$res = $connection->query("CALL createInvestment('$username','$symbol',$amount,'$year_mon_day',$curr_value)") or die(mysqli_error());
-	$qres = $res->fetch_array(MYSQLI_NUM);
-	if ($qres[0] == "0")
+	$error = 0;
+	
+	$symbol=htmlspecialchars($_POST['symbol'],ENT_QUOTES);
+	$amount=htmlspecialchars($_POST['amount'],ENT_QUOTES);
+	$curr_value=0;
+	$username= $_SESSION['username'];
+	
+	$tempCsvString = file_get_contents("http://download.finance.yahoo.com/d/quotes.csv?s=". $symbol ."&f=snp");
+	$stockData = csvToArray($tempCsvString);
+	
+	if(isset($stockData[0][2]) && is_numeric($stockData[0][2]))
 	{
-		echo "yes";
+		$curr_value = floatval($stockData[0][2]);
 	}
 	else
 	{
+		$error++;
+	}
+	
+	if (is_numeric($amount) && floatval($amount) > 0)
+	{
+		$amount = floatval($amount);
+	} 
+	else {
+		$error++;
+	}
+	
+	if ($error == 0)
+	{
+		$connection = new mysqli($serverInfo["address"], $serverInfo["username"], $serverInfo["password"]);
+		if (mysqli_connect_errno()) {
+			die('Could not connect: ' . mysqli_connect_error());
+		}
+		$connection->select_db($serverInfo["db"]);
+		$year_mon_day = date("Y-m-d");
+		$res = $connection->query("CALL createInvestment('$username','$symbol',$amount,'$year_mon_day',$curr_value)") or die(mysqli_error());
+		$qres = $res->fetch_array(MYSQLI_NUM);
+		if ($qres[0] == "0")
+		{
+			echo "yes";
+		}
+		else
+		{
+			echo "no";
+		}
+	}
+	else {
 		echo "no";
 	}
+} 
+else if($action == "del") {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 else {
 	echo "no";
 }
-
 
 
 
