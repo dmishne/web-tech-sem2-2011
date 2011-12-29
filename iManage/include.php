c1ca779c76c81d63c5c0bf586dc16d18ff70c997
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2011-12-29 16:41:30 +0000

    Fix for scrollbar problem in add income/payout

--- iManage/include.php
+++ iManage/include.php
@@ -2,9 +2,13 @@
 <link rel="stylesheet" href="Styles/menu.css" type="text/css" />
 <link rel="stylesheet" href="Styles/header.css" type="text/css" />
 <link rel="stylesheet" href="Styles/bar.css" type="text/css" />
-
+<link rel="stylesheet" href="JScrollPane/jquery.jscrollpane.css" type="text/css" />
+<link rel="stylesheet" href="JScrollPane/lorenze.css" type="text/css" />
 
 <script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="JScrollPane/jquery.jscrollpane.min.js"></script>
+<script type="text/javascript" src="JScrollPane/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="JScrollPane/mwheelIntent.js"></script>
 <script type="text/javascript" src="jquery.cookie.js"></script>
 <script type="text/javascript" src="menu.js"></script>
 <script type="text/javascript" src="init.js"></script>
