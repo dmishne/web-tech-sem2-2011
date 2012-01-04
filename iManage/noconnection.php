013b9ab6d61f9bc1a5fc17eb8f2aa5baae053530
daniel.mishne <daniel.mishne@d34c5daa-3f3e-d304-cf38-cd2a81286530>
2012-01-04 17:43:18 +0000

    add check of connection to mysql server before loading of page.

---
+++ iManage/noconnection.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title> iManage </title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<link rel="icon" href="images/logo.ico" />
+	<link rel="apple-touch-icon" href="images/icon_apple.png" />
+	<?php include "include.php"; ?>
+</head>
+
+<body>
+<div id="wrapper">
+
+	
+	
+	<div id="middle" style="width:100%;">	
+		<div id="content" style="width:100%; text-align:center;">
+			<div id="content-head">
+		           Server is down.
+			</div>
+			<div id="content-middle">
+				   Server is down, Please try again later.
+			</div>
+		</div>
+	</div>
+	
+	
+	
+</div> <!-- wrapper -->
+</body>
+</html>
